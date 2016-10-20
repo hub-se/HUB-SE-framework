8e9c347650ffe5fc7075a8f9c0c1add6319f4917
heidensi <heiden@informatik.hu-berlin.de>
2016-10-20 15:24:39 +0200

    system utils update

--- src/main/java/se/de/hu_berlin/informatik/utils/miscellaneous/SystemUtils.java
+++ src/main/java/se/de/hu_berlin/informatik/utils/miscellaneous/SystemUtils.java
@@ -1,5 +1,10 @@
 package se.de.hu_berlin.informatik.utils.miscellaneous;
 
+import java.io.File;
+
+import se.de.hu_berlin.informatik.utils.tm.modules.ExecuteCommandInSystemEnvironmentAndReturnOutputModule;
+import se.de.hu_berlin.informatik.utils.tm.modules.ExecuteCommandInSystemEnvironmentModule;
+
 final public class SystemUtils {
 	
 	//suppress default constructor (class should not be instantiated)
@@ -27,5 +32,106 @@ final public class SystemUtils {
 		final long freeMemory = runtime.freeMemory();
 		
 		return freeMemory + (maxMemory - allocatedMemory);
+	}
+	
+	/**
+	 * Executes a given command in the system's environment, while additionally using the given Java environment,
+	 * Will abort the program in case of an error in the executed process. If any of the given Java related
+	 * Strings is null, then the command will be executed with the system's default Java environment.
+	 * @param executionDir
+	 * an execution directory in which the command shall be executed
+	 * @param javaBinDir
+	 * path to Java binaries
+	 * @param javaHomeDir
+	 * path to Java home directory
+	 * @param javaJREDir
+	 * path to a Java JRE directory
+	 * @param commandArgs
+	 * the command to execute, given as an array
+	 */
+	public static void executeCommandInJavaEnvironment(File executionDir, 
+			String javaBinDir, String javaHomeDir, String javaJREDir, String... commandArgs) {
+		int executionResult = -1;
+		if (javaBinDir == null || javaHomeDir == null || javaJREDir == null) {
+			executionResult = new ExecuteCommandInSystemEnvironmentModule(executionDir)
+					.submit(commandArgs)
+					.getResult();
+		} else {
+			executionResult = new ExecuteCommandInSystemEnvironmentModule(executionDir, javaBinDir)
+					.setEnvVariable("JAVA_HOME", javaHomeDir)
+					.setEnvVariable("JRE_HOME", javaJREDir)
+					.submit(commandArgs)
+					.getResult();
+		}
+		
+		if (executionResult != 0) {
+			Log.abort(SystemUtils.class, "Error while executing command: " + Misc.arrayToString(commandArgs, " ", "", ""));
+		}
+	}
+	
+	/**
+	 * Executes a given command in the system's environment, while additionally using the given Java environment,
+	 * Will abort the program in case of an error in the executed process. The command will be executed with 
+	 * the system's default Java environment.
+	 * @param executionDir
+	 * an execution directory in which the command shall be executed
+	 * @param commandArgs
+	 * the command to execute, given as an array
+	 */
+	public static void executeCommand(File executionDir, String... commandArgs) {
+		executeCommandInJavaEnvironment(executionDir, null, null, null, commandArgs);
+	}
+	
+	/**
+	 * Executes a given command in the system's environment, while additionally using a the given Java environment. 
+	 * Returns either the process' output to standard out or to error out. If any of the given Java related
+	 * Strings is null, then the command will be executed with the system's default Java environment.
+	 * @param executionDir
+	 * an execution directory in which the command shall be executed
+	 * @param returnErrorOutput
+	 * whether to output the error output channel instead of standeard out
+	 * @param javaBinDir
+	 * path to Java binaries
+	 * @param javaHomeDir
+	 * path to Java home directory
+	 * @param javaJREDir
+	 * path to a Java JRE directory
+	 * @param commandArgs
+	 * the command to execute, given as an array
+	 * @return
+	 * the process' output to standard out or to error out
+	 */
+	public static String executeCommandWithOutputInJavaEnvironment(File executionDir, boolean returnErrorOutput, 
+			String javaBinDir, String javaHomeDir, String javaJREDir, String... commandArgs) {
+		if (javaBinDir == null || javaHomeDir == null || javaJREDir == null) {
+			return new ExecuteCommandInSystemEnvironmentAndReturnOutputModule(executionDir, returnErrorOutput)
+					.submit(commandArgs)
+					.getResult();
+		} else {
+			return new ExecuteCommandInSystemEnvironmentAndReturnOutputModule(executionDir, returnErrorOutput, javaBinDir)
+					.setEnvVariable("JAVA_HOME", javaHomeDir)
+					.setEnvVariable("JRE_HOME", javaJREDir)
+					.submit(commandArgs)
+					.getResult();
+		}
+	}
+	
+	/**
+	 * Executes a given command in the system's environment, while additionally using a the given Java environment. 
+	 * Returns either the process' output to standard out or to error out. The command will be executed with 
+	 * the system's default Java environment.
+	 * @param executionDir
+	 * an execution directory in which the command shall be executed
+	 * @param returnErrorOutput
+	 * whether to output the error output channel instead of standeard out
+	 * @param commandArgs
+	 * the command to execute, given as an array
+	 * @return
+	 * the process' output to standard out or to error out
+	 */
+	public static String executeCommandWithOutput(File executionDir, boolean returnErrorOutput, 
+			String... commandArgs) {
+		return executeCommandWithOutputInJavaEnvironment(executionDir, returnErrorOutput, 
+				null, null, null, commandArgs);
 	}
 }
