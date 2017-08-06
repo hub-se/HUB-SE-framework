d1f80b3115cb65e338031bb0e5d1c7a1e004dba2
heidensi <heiden@informatik.hu-berlin.de>
2017-08-06 16:10:37 +0200

    added excluded class names to parent last classloader

--- src/main/java/se/de/hu_berlin/informatik/utils/miscellaneous/ParentLastClassLoader.java
+++ src/main/java/se/de/hu_berlin/informatik/utils/miscellaneous/ParentLastClassLoader.java
@@ -9,25 +9,25 @@ public class ParentLastClassLoader extends URLClassLoader {
 
 	private ChildClassLoader childClassLoader;
 
-	public ParentLastClassLoader(List<URL> classpath, ClassLoader parent, boolean debug) {
-		this(classpath.toArray(new URL[classpath.size()]), parent, debug);
+	public ParentLastClassLoader(List<URL> classpath, ClassLoader parent, boolean debug, String... excludes) {
+		this(classpath.toArray(new URL[classpath.size()]), parent, debug, (String[]) excludes);
 	}
 
-	public ParentLastClassLoader(URL[] classpath, ClassLoader parent, boolean debug) {
+	public ParentLastClassLoader(URL[] classpath, ClassLoader parent, boolean debug, String... excludes) {
 		super(new URL[0], parent);
 		if (debug) {
-			childClassLoader = new DebugChildClassLoader(classpath, new DetectClass(this.getParent()));
+			childClassLoader = new DebugChildClassLoader(classpath, new DetectClass(this.getParent()), (String[]) excludes);
 		} else {
-			childClassLoader = new ChildClassLoader(classpath, new DetectClass(this.getParent()));
+			childClassLoader = new ChildClassLoader(classpath, new DetectClass(this.getParent()), (String[]) excludes);
 		}
 	}
 
-	public ParentLastClassLoader(List<URL> classpath, boolean debug) {
-		this(classpath.toArray(new URL[classpath.size()]), Thread.currentThread().getContextClassLoader(), debug);
+	public ParentLastClassLoader(List<URL> classpath, boolean debug, String... excludes) {
+		this(classpath.toArray(new URL[classpath.size()]), Thread.currentThread().getContextClassLoader(), debug, excludes);
 	}
 
-	public ParentLastClassLoader(URL[] classpath, boolean debug) {
-		this(classpath, Thread.currentThread().getContextClassLoader(), debug);
+	public ParentLastClassLoader(URL[] classpath, boolean debug, String... excludes) {
+		this(classpath, Thread.currentThread().getContextClassLoader(), debug, excludes);
 	}
 
 	@Override
@@ -41,8 +41,8 @@ public class ParentLastClassLoader extends URLClassLoader {
 
 	private static class DebugChildClassLoader extends ChildClassLoader {
 
-		public DebugChildClassLoader(URL[] urls, DetectClass realParent) {
-			super(urls, realParent);
+		public DebugChildClassLoader(URL[] urls, DetectClass realParent, String... excludes) {
+			super(urls, realParent, (String[]) excludes);
 		}
 
 		@Override
@@ -53,6 +53,20 @@ public class ParentLastClassLoader extends URLClassLoader {
 				Log.out(this, "Found loaded class: '%s'.", name);
 				Log.out(this, "Found loaded class path: '%s'.", 
 						loaded.getResource(loaded.getSimpleName() + ".class"));
+				return loaded;
+			}
+			
+			if (isExcluded(name)) {
+				Log.out(this, "Class excluded: '%s'.", name);
+				try {
+					loaded = getRealParent().loadClass(name);
+					Log.out(this, "Loaded class from parent: '%s'.", name);
+					Log.out(this, "Loaded class path from parent: '%s'.",
+							loaded.getResource(loaded.getSimpleName() + ".class"));
+				} catch (ClassNotFoundException x) {
+					Log.out(this, "Loading class from super: '%s'.", name);
+					throw x;
+				}
 				return loaded;
 			}
 
@@ -81,14 +95,16 @@ public class ParentLastClassLoader extends URLClassLoader {
 	private static class ChildClassLoader extends URLClassLoader {
 
 		private DetectClass realParent;
+		private String[] excludes;
 
 		public DetectClass getRealParent() {
 			return realParent;
 		}
 
-		public ChildClassLoader(URL[] urls, DetectClass realParent) {
+		public ChildClassLoader(URL[] urls, DetectClass realParent, String... excludes) {
 			super(urls, null);
 			this.realParent = realParent;
+			this.excludes = excludes;
 		}
 
 		public Class<?> findClassinSuper(String name) throws ClassNotFoundException {
@@ -105,12 +121,25 @@ public class ParentLastClassLoader extends URLClassLoader {
 			if (loaded != null) {
 				return loaded;
 			}
+			
+			if (isExcluded(name)) {
+				return realParent.loadClass(name);
+			}
 
 			try {
 				return findClassinSuper(name);
 			} catch (ClassNotFoundException e) {
 				return realParent.loadClass(name);
 			}
+		}
+
+		public boolean isExcluded(String name) {
+			for (String exclude : excludes) {
+				if (name.startsWith(exclude)) {
+					return true;
+				}
+			}
+			return false;
 		}
 
 	}
