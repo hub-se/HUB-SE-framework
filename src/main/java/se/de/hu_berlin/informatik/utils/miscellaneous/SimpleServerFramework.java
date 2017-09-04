50c4955db2b7270f0bfb46357a73d4a5a203dedd
heidensi <heiden@informatik.hu-berlin.de>
2017-09-05 01:37:58 +0200

    make server framework more simple

--- src/main/java/se/de/hu_berlin/informatik/utils/miscellaneous/SimpleServerFramework.java
+++ src/main/java/se/de/hu_berlin/informatik/utils/miscellaneous/SimpleServerFramework.java
@@ -9,12 +9,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Random;
-import java.util.function.BiPredicate;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 public class SimpleServerFramework {
+	
+	final private static byte NULL_DATA = 2;
+	final private static byte NORMAL_DATA = 3;
 
 	public static int getFreePort() {
 		return getFreePort(new Random().nextInt(60536) + 5000);
@@ -72,9 +71,8 @@ public class SimpleServerFramework {
 	}
 
 	public static <T extends Serializable, R extends Serializable> ServerSideListener<T, R> startServerListener(
-			ServerSocket socket, Object receiveLock, Function<T, R> sendOnReceivedData, Supplier<R> sendOnFailure) {
-		ServerSideListener<T, R> serverSideListener = new ServerSideListener<>(socket, receiveLock, sendOnReceivedData,
-				sendOnFailure);
+			ServerSocket socket, Object receiveLock) {
+		ServerSideListener<T, R> serverSideListener = new ServerSideListener<>(socket, receiveLock);
 		serverSideListener.run();
 		return serverSideListener;
 	}
@@ -83,21 +81,15 @@ public class SimpleServerFramework {
 
 		final private ServerSocket serverSocket;
 		private final Object receiveLock;
-		private final Function<T, R> sendOnReceivedData;
-		private final Supplier<R> sendOnFailure;
 
 		private Thread runningThread = null;
 		private T data;
-		private boolean isShutdown = false;
-		private boolean hasNewData = false;
-		private boolean serverErrorOccurred = false;
+		private volatile boolean isShutdown = false;
+		private volatile boolean hasNewData = false;
 
-		public ServerSideListener(ServerSocket serverSocket, Object receiveLock, Function<T, R> sendOnReceivedData,
-				Supplier<R> sendOnFailure) {
+		public ServerSideListener(ServerSocket serverSocket, Object receiveLock) {
 			this.serverSocket = serverSocket;
 			this.receiveLock = receiveLock;
-			this.sendOnReceivedData = sendOnReceivedData;
-			this.sendOnFailure = sendOnFailure;
 		}
 
 		public void run() {
@@ -113,86 +105,53 @@ public class SimpleServerFramework {
 		private void listenOnSocket(ServerSocket serverSocket) {
 			while (!isShutdown) {
 				// Create the Client Socket
-				try (Socket clientSocket = serverSocket.accept()) {
+				Socket clientSocket = null;
+				try {
+					clientSocket = serverSocket.accept();
 //					 Log.out(this, "Server Socket Extablished...");
 					// Create input and output streams to client
 					ObjectOutputStream outToClient = new ObjectOutputStream(clientSocket.getOutputStream());
 					ObjectInputStream inFromClient = new ObjectInputStream(clientSocket.getInputStream());
 
-					boolean read = false;
-					try {
-						/* Retrieve information */
-						this.data = (T) inFromClient.readObject();
-						read = true;
-					} catch (IOException e) {
-						Log.err("server", e, "error: " + e.getMessage());
-					}
-
-					// Log.out(this, "read: %b", read);
-					int tries = 0;
-					while (!read && tries < 10) {
-						++tries;
-						// Log.out(this, "try: %d", tries);
-						try {
-							/* Tell the client to send the data again... */
-							outToClient.writeObject(sendOnFailure.get());
-						} catch (IOException e) {
-							Log.err("server", e, "error: " + e.getMessage());
-							continue;
-						}
-
-						try {
-							/* Retrieve information */
-							this.data = (T) inFromClient.readObject();
-							read = true;
-						} catch (IOException e) {
-							Log.err("server", e, "error: " + e.getMessage());
-						}
-					}
-
-					if (!read) {
-						// tell any waiting threads that there is an error...
-						synchronized (receiveLock) {
-							serverErrorOccurred = true;
-							receiveLock.notifyAll();
-						}
-						// cut the connection...
-						clientSocket.close();
+//					Log.out("server", "reading data from port %d...", clientSocket.getLocalPort());
+					/* Retrieve data */
+					byte status = inFromClient.readByte();
+					if (status == NULL_DATA) {
+						this.data = null;
 					} else {
-						// tell any waiting threads that there is new data...
-						synchronized (receiveLock) {
-							hasNewData = true;
-							receiveLock.notifyAll();
-						}
-
-						boolean sent = false;
-						tries = 0;
-						while (!sent && tries < 10) {
-							// Log.out(this, "sent: %b", sent);
-							++tries;
-							try {
-								/* Send a message object back */
-								outToClient.writeObject(sendOnReceivedData.apply(this.data));
-								sent = true;
-							} catch (IOException e) {
-								Log.err("server", e, "error: " + e.getMessage());
-							}
-						}
-
-						if (!sent) {
-							// cut the connection...
-							clientSocket.close();
-						}
+						this.data = (T) inFromClient.readObject();
 					}
-				} catch (Exception e) {
-					// tell any waiting threads that there is an error...
+					
+//					Log.out("server", "writing data to port %d...", clientSocket.getLocalPort());
+					if (data == null) {
+						/* we received null data */
+						outToClient.writeByte(NULL_DATA);
+					} else {
+						/* we received data other than null (TODO: checksum or something...?) */
+						outToClient.writeByte(NORMAL_DATA);
+					}
+					outToClient.flush();
+					
+//					Log.out("server", "written data to port %d...", clientSocket.getLocalPort());
+					// tell any waiting threads that there is new data...
 					synchronized (receiveLock) {
-						serverErrorOccurred = true;
+						hasNewData = true;
 						receiveLock.notifyAll();
 					}
+					
+				} catch (Exception e) {
+					if (clientSocket != null) {
+						try {
+							clientSocket.close();
+						} catch (IOException e1) {
+							// do nothing...
+						}
+					}
+					// if any exception occurred, the client should now try to send the data again
 					Log.err("server", e, "error: " + e.getMessage());
 				}
 			}
+			
 		}
 
 		public void shutDown() {
@@ -202,11 +161,7 @@ public class SimpleServerFramework {
 				int count = 0;
 				while (!received && count < 5) {
 					++count;
-					received = sendToServer(null, serverSocket.getLocalPort(), 1, (r) -> {
-						return false;
-					}, (t, r) -> {
-						return true;
-					});
+					received = sendToServer(null, serverSocket.getLocalPort(), 1);
 				}
 				if (received) {
 					while (this.runningThread.isAlive()) {
@@ -227,10 +182,6 @@ public class SimpleServerFramework {
 			}
 		}
 
-		public boolean serverErrorOccurred() {
-			return serverErrorOccurred;
-		}
-
 		public boolean hasNewData() {
 			return hasNewData;
 		}
@@ -241,18 +192,15 @@ public class SimpleServerFramework {
 		}
 
 		public void resetListener() {
-			serverErrorOccurred = false;
 			hasNewData = false;
 		}
 
 	}
 
-	@SuppressWarnings("unchecked")
-	public static <T extends Serializable, R extends Serializable> boolean sendToServer(T data, int port,
-			int maxTryCount, Predicate<R> sendAgain, BiPredicate<T, R> isSuccessful) {
-		boolean succeeded = false;
+	public static <T extends Serializable, R extends Serializable> boolean sendToServer(
+			T data, int port, int maxTryCount) {
 		int count = 0;
-		while (!succeeded && count < maxTryCount) {
+		while (count < maxTryCount) {
 			++count;
 			// Create the socket
 			try (Socket clientSocket = new Socket((String) null, port)) {
@@ -261,61 +209,33 @@ public class SimpleServerFramework {
 				ObjectOutputStream outToServer = new ObjectOutputStream(clientSocket.getOutputStream());
 				ObjectInputStream inFromServer = new ObjectInputStream(clientSocket.getInputStream());
 
-				boolean sent = false;
-				int tries = 0;
-				while (!sent && tries < 10) {
-					++tries;
-					try {
-						// Log.out("client", "writing data to port %d...",
-						// port);
-						/* Send the Message Object to the server */
-						outToServer.writeObject(data);
-						sent = true;
-					} catch (IOException e) {
-						Log.err("client", e, "error: " + e.getMessage());
-					}
+//				Log.out("client", "writing data to port %d...", port);
+				/* Send the Message Object to the server */
+				if (data == null) {
+					outToServer.writeByte(NULL_DATA);
+				} else {
+					outToServer.writeByte(NORMAL_DATA);
+					outToServer.writeObject(data);
 				}
-
-				// Log.out("client", "sent: %b", sent);
-				// only wait for messages if actually sent something...
-				if (sent) {
-					R inFromServerMsg = null;
-					boolean read = false;
-					tries = 0;
-					while (!read && tries < 10) {
-						++tries;
-						try {
-							/* Retrieve the Message Object from server */
-							inFromServerMsg = (R) inFromServer.readObject();
-							read = true;
-						} catch (IOException e) {
-							Log.err("client", e, "error: " + e.getMessage());
-						}
-					}
-					// Log.out("client", "read: %b", read);
-
-					if (read) {
-						/* Print out the received Message */
-						// Log.out("client", "Message from server: " +
-						// inFromServerMsg);
-						// check if the server wants us to check the data again
-						// (could be the case that there was an exception while
-						// reading the input stream)
-						if (sendAgain.test(inFromServerMsg)) {
-							// try again
-							continue;
-						}
-						// check if the message from the server declares the
-						// transmission as successful
-						succeeded = isSuccessful.test(data, inFromServerMsg);
-					}
+				outToServer.flush();
+				
+//				Log.out("client", "reading data to port %d...", port);
+				/* Retrieve the status byte from server */
+				byte status = inFromServer.readByte();
+				
+				if ((data == null && status == NULL_DATA) ||
+						(data != null && status == NORMAL_DATA)) {
+//					Log.out("client", "read data to port %d...", port);
+					return true;
+				} else {
+					Log.err("client", "Error sending correct data: try %d", count);
 				}
 
 			} catch (Exception e) {
-				Log.err("client", e, "error: " + e.getMessage());
+				Log.err("client", e, "Try %d, error: %s", count, e.getMessage());
 			}
 		}
 
-		return succeeded;
+		return false;
 	}
 }
