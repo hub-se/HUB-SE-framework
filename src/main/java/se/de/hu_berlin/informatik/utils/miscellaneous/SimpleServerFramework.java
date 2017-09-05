ac8a82f1131c098d32c9e9c9441cf0645671150d
heidensi <heiden@informatik.hu-berlin.de>
2017-09-05 14:23:52 +0200

    server framework made simpler and more secure

--- src/main/java/se/de/hu_berlin/informatik/utils/miscellaneous/SimpleServerFramework.java
+++ src/main/java/se/de/hu_berlin/informatik/utils/miscellaneous/SimpleServerFramework.java
@@ -9,16 +9,38 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
+import java.util.Objects;
 import java.util.Random;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
+/**
+ * Simple server/client framework that starts a server socket and a listener
+ * thread on the receiving end and one may then send data through a socket
+ * connection to this server. The receiving application may block until it gets
+ * correct data, while the server listener thread runs in the background.
+ * 
+ * <p>
+ * The protocol used consists of: 1. the client sending data, 2. the server
+ * sending a response based on the data received, 3. the client either accepts
+ * or rejects the response, and 4. the server makes the correct data available
+ * and sends an acceptance message to the client to inform it that everything
+ * went correctly.
+ * 
+ * <p>
+ * In the case of errors, the data should be tried to sent again.
+ * 
+ * 
+ * @author Simon
+ *
+ */
 public class SimpleServerFramework {
-	
+
 	final private static byte ACCEPT = 0;
-	final private static byte REJECT = 1;
-	
+
 	final private static byte NULL_DATA = 2;
 	final private static byte NORMAL_DATA = 3;
-	
+
 	final private static byte RESET = 4;
 
 	public static int getFreePort() {
@@ -55,20 +77,20 @@ public class SimpleServerFramework {
 		return currentPort;
 	}
 
-	public static ServerSocket startServer() {
+	public static <T extends Serializable, R extends Serializable> ServerSideListener<T, R> startServer() {
 		return _startServer(getFreePort());
 	}
 
-	public static ServerSocket startServer(int port) {
+	public static <T extends Serializable, R extends Serializable> ServerSideListener<T, R> startServer(int port) {
 		return _startServer(getFreePort(port));
 	}
 
-	private static ServerSocket _startServer(int port) {
+	private static <T extends Serializable, R extends Serializable> ServerSideListener<T, R> _startServer(int port) {
 		try {
 			ServerSocket socket = new ServerSocket(port);
 			// set a timeout to stop blocking in case of errors occurring...
 			// socket.setSoTimeout(10000);
-			return socket;
+			return startServerListener(socket);
 		} catch (Exception e) {
 			Log.err("server", e, "error: " + e.getMessage());
 		}
@@ -76,9 +98,9 @@ public class SimpleServerFramework {
 		return null;
 	}
 
-	public static <T extends Serializable, R extends Serializable> ServerSideListener<T, R> startServerListener(
-			ServerSocket socket, Object receiveLock) {
-		ServerSideListener<T, R> serverSideListener = new ServerSideListener<>(socket, receiveLock);
+	private static <T extends Serializable, R extends Serializable> ServerSideListener<T, R> startServerListener(
+			ServerSocket socket) {
+		ServerSideListener<T, R> serverSideListener = new ServerSideListener<>(socket);
 		serverSideListener.run();
 		return serverSideListener;
 	}
@@ -86,19 +108,19 @@ public class SimpleServerFramework {
 	public static class ServerSideListener<T extends Serializable, R extends Serializable> {
 
 		final private ServerSocket serverSocket;
-		private final Object receiveLock;
+		private final Semaphore lock = new Semaphore(1);
 
 		private Thread runningThread = null;
 		private T data;
 		private volatile boolean isShutdown = false;
 		private volatile boolean hasNewData = false;
 
-		public ServerSideListener(ServerSocket serverSocket, Object receiveLock) {
-			this.serverSocket = serverSocket;
-			this.receiveLock = receiveLock;
+		public ServerSideListener(ServerSocket serverSocket) {
+			this.serverSocket = Objects.requireNonNull(serverSocket);
 		}
 
-		public void run() {
+		// set protected to prevent unintended usage
+		protected void run() {
 			if (this.runningThread == null || !this.runningThread.isAlive()) {
 				this.runningThread = new Thread(() -> {
 					listenOnSocket(serverSocket);
@@ -112,14 +134,22 @@ public class SimpleServerFramework {
 			while (!isShutdown) {
 				// Create the Client Socket
 				try (Socket clientSocket = serverSocket.accept()) {
+					// the timeout only matters AFTER a connection is
+					// established
 					clientSocket.setSoTimeout(10000);
-//					 Log.out(this, "Server Socket Extablished...");
+
+					// Log.out(this, "Server Socket Extablished...");
 					// Create input and output streams to client
 					ObjectOutputStream outToClient = new ObjectOutputStream(clientSocket.getOutputStream());
 					ObjectInputStream inFromClient = new ObjectInputStream(clientSocket.getInputStream());
 
+					lock.acquireUninterruptibly();
 					try {
-//						Log.out("server", "reading data from port %d...", clientSocket.getLocalPort());
+						/*-----------------------------------------
+						 * server expects to receive data
+						 * --------------------------------------*/
+						// Log.out("server", "reading data from port %d...",
+						// clientSocket.getLocalPort());
 						/* Retrieve data */
 						byte status = inFromClient.readByte();
 						if (status == RESET) {
@@ -130,43 +160,70 @@ public class SimpleServerFramework {
 						} else {
 							this.data = (T) inFromClient.readObject();
 						}
-						
-//						Log.out("server", "writing data to port %d...", clientSocket.getLocalPort());
+
+						/*-----------------------------------------
+						 * server sends response to client 
+						 * based on data
+						 * --------------------------------------*/
+						// Log.out("server", "writing data to port %d...",
+						// clientSocket.getLocalPort());
 						if (data == null) {
 							/* we received null data */
 							outToClient.writeByte(NULL_DATA);
 						} else {
-							/* we received data other than null (TODO: checksum or something...?) */
+							/*
+							 * we received data other than null (TODO: checksum
+							 * or something...?)
+							 */
 							outToClient.writeByte(NORMAL_DATA);
 						}
 						outToClient.flush();
-						
-//						Log.out("server", "reading data from port %d...", clientSocket.getLocalPort());
+
+						/*-----------------------------------------
+						 * server expects ACC response from client;
+						 * on reception, the server knows that the
+						 * received data was (probably) correct
+						 * --------------------------------------*/
+						// Log.out("server", "reading data from port %d...",
+						// clientSocket.getLocalPort());
 						/* Retrieve data */
 						status = inFromClient.readByte();
 						if (status == RESET) {
 							continue;
 						}
-						
+
 						if (status == ACCEPT) {
-//							Log.out("server", "written data to port %d...", clientSocket.getLocalPort());
-							// tell any waiting threads that there is new data...
-							synchronized (receiveLock) {
-								hasNewData = true;
-								receiveLock.notifyAll();
-							}
+							/*-----------------------------------------
+							 * server sends ACC response to client and
+							 * may now notify any waiting threads that
+							 * there is new data
+							 * --------------------------------------*/
+							outToClient.writeByte(ACCEPT);
+							outToClient.flush();
+							// tell any waiting threads that there is new
+							// data...
+							hasNewData = true;
+						} else {
+							/*-----------------------------------------
+							 * should not occur, but send reset message
+							 * --------------------------------------*/
+							outToClient.writeByte(RESET);
+							outToClient.flush();
 						}
 					} catch (SocketTimeoutException e) {
 						Log.err("server", "Timeout!");
 						outToClient.writeByte(RESET);
 						outToClient.flush();
+					} finally {
+						lock.release();
 					}
 				} catch (Exception e) {
-					// if any exception occurred, the client should now try to send the data again
+					// if any exception occurred, the client should now try to
+					// send the data again
 					Log.err("server", e, "error: " + e.getMessage());
 				}
 			}
-			
+
 		}
 
 		public void shutDown() {
@@ -189,11 +246,54 @@ public class SimpleServerFramework {
 				} else {
 					// just don't wait?...
 				}
+			}
+			try {
+				serverSocket.close();
+			} catch (IOException e) {
+				// don't care
+			}
+		}
+
+		public T getNewData() {
+			return getNewData(Long.MAX_VALUE);
+		}
+
+		public T getNewData(long timeout) {
+			// Log.out(this, "waiting for data...");
+			// wait for new data if necessary (should already be available)
+			boolean noTimeoutOccurred = true;
+			T result = null;
+			while (!hasNewData && noTimeoutOccurred) {
 				try {
-					serverSocket.close();
-				} catch (IOException e) {
-					// don't care
+					noTimeoutOccurred = lock.tryAcquire(timeout, TimeUnit.MILLISECONDS);
+				} catch (InterruptedException e) {
+					// try again
+				} finally {
+					if (!hasNewData && noTimeoutOccurred) {
+						lock.release();
+						try {
+							Thread.sleep(500);
+						} catch (InterruptedException e1) {
+							// do nothing...
+						}
+					}
 				}
+			}
+			// even if a timeout occurred, new data may have been made available
+			// just in this moment...
+			if (hasNewData) {
+				result = data;
+				data = null;
+				hasNewData = false;
+				if (noTimeoutOccurred) {
+					// only need to release the lock
+					// in this case...
+					lock.release();
+				}
+				return result;
+			} else {
+				// no new data
+				return null;
 			}
 		}
 
@@ -210,10 +310,14 @@ public class SimpleServerFramework {
 			hasNewData = false;
 		}
 
+		public int getServerPort() {
+			return serverSocket.getLocalPort();
+		}
+
 	}
 
-	public static <T extends Serializable, R extends Serializable> boolean sendToServer(
-			T data, int port, int maxTryCount) {
+	public static <T extends Serializable, R extends Serializable> boolean sendToServer(T data, int port,
+			int maxTryCount) {
 		int count = 0;
 		while (count < maxTryCount) {
 			++count;
@@ -226,7 +330,10 @@ public class SimpleServerFramework {
 				ObjectInputStream inFromServer = new ObjectInputStream(clientSocket.getInputStream());
 
 				try {
-//					Log.out("client", "writing data to port %d...", port);
+					/*-----------------------------------------
+					 * client sends data to server
+					 * --------------------------------------*/
+					// Log.out("client", "writing data to port %d...", port);
 					/* Send the Message Object to the server */
 					if (data == null) {
 						outToServer.writeByte(NULL_DATA);
@@ -236,23 +343,49 @@ public class SimpleServerFramework {
 					}
 					outToServer.flush();
 
-//					Log.out("client", "reading data from port %d...", port);
+					/*-----------------------------------------
+					 * client expects respective response 
+					 * from server
+					 * --------------------------------------*/
+					// Log.out("client", "reading data from port %d...", port);
 					/* Retrieve the status byte from server */
 					byte status = inFromServer.readByte();
 					if (status == RESET) {
 						continue;
 					}
 
-					if ((data == null && status == NULL_DATA) ||
-							(data != null && status == NORMAL_DATA)) {
-//						Log.out("client", "read data from port %d...", port);
+					// Log.out("client", "read data from port %d...", port);
+					if ((data == null && status == NULL_DATA) || (data != null && status == NORMAL_DATA)) {
+						/*-----------------------------------------
+						 * client acknowledges that server has
+						 * received the correct data
+						 * --------------------------------------*/
 						outToServer.writeByte(ACCEPT);
 						outToServer.flush();
-						return true;
 					} else {
+						/*-----------------------------------------
+						 * client wants to start the process over
+						 * --------------------------------------*/
 						Log.err("client", "Error sending correct data: try %d", count);
-						outToServer.writeByte(REJECT);
+						outToServer.writeByte(RESET);
 						outToServer.flush();
+						continue;
+					}
+
+					/*-----------------------------------------
+					 * client expects ACC response from server,
+					 * so that it can be sure that the server
+					 * provided the new data
+					 * --------------------------------------*/
+					// Log.out("client", "reading data from port %d...", port);
+					/* Retrieve the status byte from server */
+					status = inFromServer.readByte();
+					if (status == RESET) {
+						continue;
+					}
+
+					if (status == ACCEPT) {
+						return true;
 					}
 
 				} catch (SocketTimeoutException e) {
