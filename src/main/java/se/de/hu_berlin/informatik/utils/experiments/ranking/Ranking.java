01174055547d6b615f948ff990e3aff5e1ba4f58
Simon Heiden <heiden@informatik.hu-berlin.de>
2019-10-10 15:23:51 +0200

    very small refactoring

--- src/main/java/se/de/hu_berlin/informatik/utils/experiments/ranking/Ranking.java
+++ src/main/java/se/de/hu_berlin/informatik/utils/experiments/ranking/Ranking.java
@@ -7,7 +7,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
